df810d611a65ee20cfabc810e4324c3d526628c3
Claire P <claire.peracchio@gmail.com>
2020-09-16 11:46:57 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,97 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from datetime import datetime
+from flask_migrate import Migrate
+import dateutil.parser
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from flask_wtf import Form
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#db.create_all(app)
+#migrate = Migrate(app, db)
+#db_uri = environ.get('postgres://claireperacchio@localhost:5432/fyyur')
+#engine = create_engine(db_uri, echo=True)
+
+# TODO: connect to a local postgresql database
+#Base.metadata.create_all(engine)
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+# Venue model
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean, default=True)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='venue')
+
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
+
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# Artist model
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean, default=True)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='artist')
+
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+# Show model
+class Show(db.Model):
+  __tablename__ = 'Show'
+  """docstring for ClassName"""
+
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+  start_time = db.Column(db.DateTime, default=datetime.now)
+
+
+  def __repr__(self):
+    return f'<Show {self.id}, Artist {self.artist_id}, Venue {self.venue_id}>'
